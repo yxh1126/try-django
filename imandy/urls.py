6f6e34fb350f4fa1df66d1ea63d4a55c6dd18dea
Timothy <behuangyi@gmail.com>
2021-04-17 17:07:25 -0700

    Added Login Functionality

--- imandy/urls.py
+++ imandy/urls.py
@@ -23,5 +23,5 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
